7b6183ceeb4f049041a1fdbb99505f69453d3ce1
maxchiang <qoq8140555@gmail.com>
2025-12-06 19:59:41 +0800

    feat(app): Add MainViewModel and TheWeatherForecastApp
    Adds the following files:
    - MainViewModel.kt
    - TheWeatherForecastApp.kt
    - navigation/
    
    Updates the following files:
    - build.gradle.kts
    - AndroidManifest.xml
    - MainActivity.kt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "TheWeatherForecastApp"
 include(":app")
- 
+include(":core:network")
+include(":feature:weather")
+include(":feature:home")
+include(":core:domain")
+include(":core:data")
+include(":core:ui")

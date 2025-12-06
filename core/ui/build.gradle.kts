78aa60d2a1b135e013b7a46277aea74eb330d286
maxchiang <qoq8140555@gmail.com>
2025-12-06 19:55:08 +0800

    feat(core/ui): Move UI theme files and set up initial structure
    Moves Color.kt, Theme.kt, and Type.kt from the app module.
    Also adds build.gradle.kts and a mapper directory to establish the foundation of the core/ui module.

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.max.theweatherforecastapp.core.ui"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 29
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    api(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.material3)
+}
